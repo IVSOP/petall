7d23d7b3f8394c16bf677515f86f716d8e8f3169
DigoqueDigo <diogoacmarques2003@gmail.com>
2025-08-16 19:22:44 +0100

    paginated query for participant transfers

--- backend/src/participant.rs
+++ backend/src/participant.rs
@@ -1,6 +1,11 @@
-use crate::AppState;
+use crate::{AppState};
 use serde::{Deserialize, Serialize};
+use crate::router::EnergyTransferQuery;
+use bigdecimal::BigDecimal;
+use chrono::NaiveDateTime;
 use uuid::Uuid;
+
+const TRANSFERS_PER_PAGE: u32 = 10;
 
 #[derive(Debug, Deserialize, Serialize, sqlx::Type)]
 #[sqlx(type_name = "participant_role", rename_all = "lowercase")]
@@ -22,6 +27,17 @@ pub struct Participant {
 pub struct ParticipantCommunity {
     pub participant_id: Uuid,
     pub community_id: Uuid,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct EnergyTransfer {
+    pub id: Uuid,
+    pub participant_from: Uuid,
+    pub participant_to: Uuid,
+    pub community: Uuid,
+    pub energy_wh: BigDecimal,
+    pub start: NaiveDateTime,
+    pub end: NaiveDateTime,
 }
 
 impl AppState {
@@ -59,6 +75,24 @@ impl AppState {
             WHERE participant_id = $1
             "#,
             participant_id
+        )
+        .fetch_all(&self.pg_pool)
+        .await
+    }
+
+    pub async fn get_participant_energytransfer(&self, participant_id: &Uuid, community_id: &Uuid, query: EnergyTransferQuery) -> sqlx::Result<Vec<EnergyTransfer>> {
+        sqlx::query_as!(
+            EnergyTransfer,
+            r#"
+            SELECT * FROM energytransfer
+            WHERE (participant_from = $1 OR participant_to = $1) AND community = $2
+            ORDER BY start ASC
+            LIMIT $3 OFFSET $4
+            "#,
+            participant_id,
+            community_id,
+            TRANSFERS_PER_PAGE as i64,
+            (TRANSFERS_PER_PAGE * (query.page - 1)) as i64
         )
         .fetch_all(&self.pg_pool)
         .await
--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,6 +1,6 @@
 use crate::AppState;
-use crate::community::Community;
 use crate::error::{AppError, AppResult};
+use crate::community::Community;
 use axum::{
     Json, Router, debug_handler,
     extract::{Path, State},
@@ -17,6 +17,7 @@ pub fn router(state: AppState) -> Router {
         .route("/community/register", post(register_community))
         .route("/participant/{participant_id}", get(get_participant))
         .route("/participant/communities/{participant_id}", get(get_participant_communities))
+        .route("/energytransfer/{community_id}/{participant_id}", get(get_participant_energytransfer))
         .with_state(state)
         .layer(TraceLayer::new_for_http())
 }
@@ -27,6 +28,13 @@ pub struct CommunityRegisterRequest {
     pub entity: String,
     pub supplier: Uuid,
 }
+
+#[derive(serde::Deserialize, Validate)]
+pub struct EnergyTransferQuery {
+    #[validate(range(min = 1))]
+    pub page: u32,
+}
+
 
 #[debug_handler]
 pub async fn get_community(
@@ -45,6 +53,7 @@ async fn register_community(
     State(state): State<AppState>,
     Json(request): Json<CommunityRegisterRequest>,
 ) -> AppResult<impl IntoResponse> {
+    request.validate()?;
     if state.get_community_by_entity(&request.entity).await?.is_some() {
         Err(AppError::CommunityEntityAlreadyInUse(request.entity))
     } else {
@@ -88,4 +97,29 @@ pub async fn get_participant_communities(
     }
 
     Ok(Json(communities))
+}
+
+
+#[debug_handler]
+pub async fn get_participant_energytransfer(
+    State(state): State<AppState>,
+    Path((community_id, participant_id)): Path<(Uuid, Uuid)>,
+    Json(query): Json<EnergyTransferQuery>,
+) -> AppResult<impl IntoResponse> {
+    query.validate()?;
+    if state.get_participant_by_id(&participant_id).await?.is_none() {
+        return Err(AppError::ParticipantNotFoundId(participant_id));
+    } 
+
+    if state.get_community_by_id(&community_id).await?.is_none() {
+        return Err(AppError::CommunityNotFound(community_id));
+    }
+
+    Ok(Json(
+        state.get_participant_energytransfer(
+            &participant_id,
+            &community_id,
+            query
+        ).await?
+    ))
 }
