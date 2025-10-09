511cf6cf823b111661e600616d4faac6a095e28b
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-10-09 13:31:35 +0100

    faltou o frontend

--- frontend/src/routes/(app)/community/[id]/+page.svelte
+++ frontend/src/routes/(app)/community/[id]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import SectionCards from '$lib/components/SectionCards.svelte';
 	import DataTable from '$lib/components/DataTable.svelte';
 	import ChartAreaInteractive from '$lib/components/ChartAreaInteractive.svelte';
+	import EnergyBlocks from './EnergyBlocks.svelte';
 
 	const { data } = $props();
 
@@ -46,7 +46,7 @@
 	<div class="flex flex-1 flex-col">
 		<div class="@container/main flex flex-1 flex-col gap-2">
 			<div class="flex flex-col gap-6">
-				<SectionCards />
+				<EnergyBlocks energyTransfers={energyRecords} />
 				<ChartAreaInteractive />
 				<DataTable data={energyRecords} bind:pageIndex bind:pageLimit bind:pageSize />
 			</div>
