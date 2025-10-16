4ecce78754744541b094dc328edefb1282f3dcf3
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-10-16 21:48:10 +0100

    stats box 2

--- frontend/src/routes/(app)/community/[id]/EnergyBlocks.svelte
+++ frontend/src/routes/(app)/community/[id]/EnergyBlocks.svelte
@@ -38,11 +38,29 @@
         for (let i = 30; i < statsLast60.length && i < 60; i++) {
             total += statsLast60[i].generatedPrice - statsLast60[i].consumedPrice;
         }
-        return total - price_last_30;
+        return total;
     })
 
     let price_last_60_to_last_30_percent = $derived.by(() => {
         return ((price_last_30 - price_last_60_to_last_30) / Math.abs(price_last_60_to_last_30)) * 100
+    })
+
+    let delta_last_30 = $derived.by(() => {
+        let total = 0;
+        for (let i = 0; i < statsLast60.length && i < 30; i++) {
+            total += statsLast60[i].generatedSum - statsLast60[i].consumedSum;
+        }
+        return total;
+    })
+    let delta_last_60_to_last_30 = $derived.by(() => {
+        let total = 0;
+        for (let i = 30; i < statsLast60.length && i < 60; i++) {
+            total += statsLast60[i].generatedSum - statsLast60[i].consumedSum;
+        }
+        return total;
+    })
+    let delta_last_60_to_last_30_percent = $derived.by(() => {
+        return ((delta_last_30 - delta_last_60_to_last_30) / Math.abs(delta_last_60_to_last_30)) * 100
     })
 
     $effect(() => {
@@ -51,6 +69,8 @@
         // console.log(`stats last 30 ${JSON.stringify(statsLast60)}`)
         console.log(`price last 60 to 30 ${JSON.stringify(price_last_60_to_last_30)}`)
         console.log(`price last 30 ${JSON.stringify(price_last_30)}`)
+        console.log(`delta last 60 to 30 ${JSON.stringify(delta_last_60_to_last_30)}`)
+        console.log(`delta last 30 ${JSON.stringify(delta_last_30)}`)
     })
 
 	//   $effect(() => {
@@ -63,64 +83,85 @@
 	class="grid grid-cols-1 gap-4 px-0 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-0 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card"
 >
     <Card.Root class="@container/card">
-	<Card.Header>
-		<Card.Description>Last 30 days balance</Card.Description>
-		<Card.Title class="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-			€{price_last_30.toFixed(2)}
-		</Card.Title>
+        <Card.Header>
+            <Card.Description>Last 30 days balance</Card.Description>
+            <Card.Title class="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                €{price_last_30.toFixed(2)}
+            </Card.Title>
 
-		<Card.Action>
-			<Badge
-				variant={price_last_60_to_last_30_percent >= 0 ? 'outline' : 'destructive'}
-				class="flex items-center gap-1"
-			>
-				{#if price_last_60_to_last_30_percent >= 0}
-					<TrendingUp class="size-4 text-green-500" />
-					+{price_last_60_to_last_30_percent.toFixed(1)}%
-				{:else}
-					<TrendingDown class="size-4 text-red-500" />
-					{price_last_60_to_last_30_percent.toFixed(1)}%
-				{/if}
-			</Badge>
-		</Card.Action>
-	</Card.Header>
+            <Card.Action>
+                <Badge
+                    variant='outline'
+                    class="flex items-center gap-1"
+                >
+                    {#if price_last_60_to_last_30_percent >= 0}
+                        <TrendingUp class="size-4 text-green-500" />
+                        +{price_last_60_to_last_30_percent.toFixed(1)}%
+                    {:else}
+                        <TrendingDown class="size-4 text-red-500" />
+                        {price_last_60_to_last_30_percent.toFixed(1)}%
+                    {/if}
+                </Badge>
+            </Card.Action>
+        </Card.Header>
 
-	<Card.Footer class="flex-col items-start gap-1.5 text-sm">
-		{#if price_last_60_to_last_30_percent >= 0}
-			<div class="line-clamp-1 flex gap-2 font-medium">
-				Trending up this month <TrendingUp class="size-4 text-green-500" />
-			</div>
-			<div class="text-muted-foreground">Performance improved</div>
-		{:else}
-			<div class="line-clamp-1 flex gap-2 font-medium">
-				Trending down this month <TrendingDown class="size-4 text-red-500" />
-			</div>
-			<div class="text-muted-foreground">Performance decreased</div>
-		{/if}
-	</Card.Footer>
-</Card.Root>
+        <Card.Footer class="flex-col items-start gap-1.5 text-sm">
+            {#if price_last_60_to_last_30_percent >= 0}
+                <div class="line-clamp-1 flex gap-2 font-medium">
+                    Trending up this month <TrendingUp class="size-4 text-green-500" />
+                </div>
+                <div class="text-muted-foreground">Performance improved</div>
+            {:else}
+                <div class="line-clamp-1 flex gap-2 font-medium">
+                    Trending down this month <TrendingDown class="size-4 text-red-500" />
+                </div>
+                <div class="text-muted-foreground">Performance decreased</div>
+            {/if}
+        </Card.Footer>
+    </Card.Root>
 
 	<Card.Root class="@container/card">
 		<Card.Header>
-			<Card.Description>New Customers</Card.Description>
+			<Card.Description>Last 30 days energy balance</Card.Description>
 			<Card.Title class="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-				1,234
+				{delta_last_30.toFixed(2)}kWh
 			</Card.Title>
+
 			<Card.Action>
-				<Badge variant="outline">
-					<TrendingDown />
-					-20%
+                <Badge
+                    variant='outline'
+                    class="flex items-center gap-1"
+                >
+                    {#if delta_last_60_to_last_30_percent >= 0}
+                        <TrendingUp class="size-4 text-green-500" />
+                        +{delta_last_60_to_last_30_percent.toFixed(1)}%
+                    {:else}
+                        <TrendingDown class="size-4 text-red-500" />
+                        {delta_last_60_to_last_30_percent.toFixed(1)}%
+                    {/if}
 				</Badge>
 			</Card.Action>
-		</Card.Header>
-		<Card.Footer class="flex-col items-start gap-1.5 text-sm">
-			<div class="line-clamp-1 flex gap-2 font-medium">
-				Down 20% this period <TrendingDown class="size-4" />
-			</div>
-			<div class="text-muted-foreground">Acquisition needs attention</div>
-		</Card.Footer>
+        </Card.Header>
+
+        <Card.Footer class="flex-col items-start gap-1.5 text-sm">
+            {#if price_last_60_to_last_30_percent >= 0}
+                <div class="line-clamp-1 flex gap-2 font-medium">
+                    Trending up this month <TrendingUp class="size-4 text-green-500" />
+                </div>
+            {:else}
+                <div class="line-clamp-1 flex gap-2 font-medium">
+                    Trending down this month <TrendingDown class="size-4 text-red-500" />
+                </div>
+                {/if}
+            {#if price_last_30 >= 0}
+                <div class="text-muted-foreground">Balance &gt; 0: More energy provided than consumed</div>
+            {:else}
+                <div class="text-muted-foreground">Balance &lt; 0: More energy consumed than provided</div>
+            {/if}
+        </Card.Footer>
 	</Card.Root>
-	<Card.Root class="@container/card">
+
+    <Card.Root class="@container/card">
 		<Card.Header>
 			<Card.Description>Active Accounts</Card.Description>
 			<Card.Title class="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
@@ -140,6 +181,7 @@
 			<div class="text-muted-foreground">Engagement exceed targets</div>
 		</Card.Footer>
 	</Card.Root>
+
 	<Card.Root class="@container/card">
 		<Card.Header>
 			<Card.Description>Growth Rate</Card.Description>
