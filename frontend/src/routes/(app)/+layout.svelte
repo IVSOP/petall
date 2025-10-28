a512c37884eaa5e4f1818fcf23f7b1e7086620ee
Pedro Carvalho <pedr.carv.13@gmail.com>
2025-10-28 18:49:38 +0000

    change favicon

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
 	import type { LayoutProps } from './$types';
 	import Avatar from '$lib/components/Avatar.svelte';
@@ -8,10 +7,6 @@
 
 	let { data, children }: LayoutProps = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <div class="flex min-h-screen flex-col bg-background">
 	<header class="w-full border-b">
@@ -25,7 +20,7 @@
 								variant="ghost"
 								class="cursor-pointer items-end bg-transparent pb-0 pl-0 text-3xl leading-none font-semibold transition-opacity hover:bg-transparent hover:opacity-70"
 							>
-								<img src={favicon} alt="PeTall" class="h-8 w-8" />
+								<img src="/favicon.ico" alt="PeTall" class="h-8 w-8" />
 								PeTall
 							</Button>
 						</NavigationMenu.Item>
